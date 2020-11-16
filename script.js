612be48b64a657489287425be14054cb9850e754
Marzuki <marzukiberg@gmail.com>
2020-11-16 17:08:56 +0700

    added stop animation, levels switcher style

--- script.js
+++ script.js
@@ -2,42 +2,51 @@ const kipas = document.querySelector(".kipas");
 const onOffText = document.querySelector(".on_off_wrapper > span");
 const onOffButton = document.querySelector(".on_off");
 const onOffCircle = document.querySelector(".on_off>.on_off_circle");
-const levels = document.querySelectorAll("#level");
+const levels = document.querySelectorAll(".level_circle");
 
 let state = {
   running: false,
   level: 1,
 };
 
-function updateState() {
-  if (!state.running) {
-    kipas.classList.remove(`level_${state.level}_animation`);
-    onOffText.innerText = "Off";
-    onOffButton.classList.remove("active");
-    onOffCircle.classList.remove("active");
-    return;
+const on = () => {
+  if (kipas.classList.contains("stop")) {
+    kipas.classList.remove("stop");
   }
   [1, 2, 3]
     .filter((x) => x !== state.level)
-    .forEach((y) => {
-      kipas.classList.remove(`level_${y}_animation`);
-    });
+    .map((y) => kipas.classList.remove(`level_${y}_animation`));
   kipas.classList.add(`level_${state.level}_animation`);
   onOffText.innerText = "On";
   onOffButton.classList.add("active");
   onOffCircle.classList.add("active");
-}
+};
+
+const off = () => {
+  kipas.classList.add("stop");
+  kipas.classList.remove(`state_${state.level}_animation`);
+  onOffText.innerText = "Off";
+  onOffButton.classList.remove("active");
+  onOffCircle.classList.remove("active");
+};
 
 function toggleOnOff() {
+  !state.running ? on() : off();
   state.running = !state.running;
-  updateState();
 }
 
-window.addEventListener("load", updateState);
 onOffButton.addEventListener("click", toggleOnOff);
-levels.forEach((item) => {
+levels.forEach((item, index) => {
   item.addEventListener("click", (e) => {
     state.level = e.target.value;
-    updateState();
+    [0, 1, 2]
+      .filter((x) => x !== state.level)
+      .map((y) => {
+        levels[y].classList.remove("active");
+      });
+    levels[state.level - 1].classList.add("active");
+    if (!state.running) return;
+    on();
   });
 });
+levels[state.level - 1].classList.add("active");
